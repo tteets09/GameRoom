3a5805abe930c7549816c0bf3f149a495e92e2b4
tmteets <tmteets@uno.edu>
2022-09-24 22:30:10 -0500

    Setting up webserver for discord bot

---
+++ src/code/functions/webpage/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const ejs = require('ejs');
+const chalk = require("chalk");
+const path = require("path");
+const bodyParser = require('body-parser');
+
+module.exports = (client) => {
+    client.runWeb = async () => {
+        //Creating the webserver
+        const app = express();
+
+        //Setting default settings
+        app.use(express.static('public'));
+        app.set('views', path.join(__dirname, '/views'));
+        app.set('view engine', 'ejs');
+        app.use(bodyParser.urlencoded({extended: true}));
+
+        //Start listening on local host 3000
+        app.listen(3000)
+
+        //Logging to console the server is running
+        console.log(chalk.green("Webserver is running..."));
+    }
+}
