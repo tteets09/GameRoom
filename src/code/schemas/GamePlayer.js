88e3a0939098a5d4cadf5fb39f5d79cfe63889fe
tmteets <tmteets@uno.edu>
2022-10-07 18:41:28 -0500

    Added gamesWon and gamesLost to data

--- src/code/schemas/GamePlayer.js
+++ src/code/schemas/GamePlayer.js
@@ -3,7 +3,9 @@ const {Schema, model} = require('mongoose');
 const GameRoomPlayerSchema = new Schema({
     _id: Schema.Types.ObjectId,
     discordId: String,
-    gamesPlayed: Number
+    gamesPlayed: Number,
+    gamesWon: Number,
+    gamesLost: Number
 });
 
 module.exports = model('GameRoomPlayer', GameRoomPlayerSchema, 'Players');
