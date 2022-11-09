64573e169553d127e3f025d8a258c80b9fe2399b
tmteets <tmteets@uno.edu>
2022-11-08 22:28:21 -0600

    Added new field for password and checked if the user enters correct password

--- src/code/commands/gameroom/joingame.js
+++ src/code/commands/gameroom/joingame.js
@@ -10,19 +10,15 @@ module.exports = {
             option
                 .setName("gameid")
                 .setDescription('The game id that allows the user to join.')
-                .setAutocomplete(true)
+                .setAutocomplete(false)
                 .setRequired(true)
-        ),
+        ).addStringOption(option =>
+            option
+                .setName("gamepassword")
+                .setDescription("The game password to join the GameRoom")
+                .setAutocomplete(false)
+                .setRequired(true)),
     async autocomplete(interaction, client){
-        const focusedValue = interaction.options.getFocused();
-        const choices = [];
-        const filtered = choices.filter((choice) =>
-            choice.startsWith(focusedValue)
-        );
-
-        await interaction.respond(
-            filtered.map((choice) => ({name: choice, value: choice}))
-        );
     },
     async execute(interaction, client){
         const EMBED_COLOR = '#9bd2fc';
@@ -38,7 +34,9 @@ module.exports = {
             });
             return;
         }
+
         const gameId = interaction.options.getString("gameid");
+        const gamePass= interaction.options.getString("gamepassword")
 
         //Checking if the player exists
         const player = await Player.findOne({discordId: interaction.user.id});
@@ -76,6 +74,34 @@ module.exports = {
             return;
         }
 
+        if(gameRoom.roomPassword !== gamePass){
+            const embed = new EmbedBuilder()
+                .setTitle('Uhoh!')
+                .setDescription('It seems you do not have the correct password')
+                .setColor(EMBED_COLOR);
+
+            interaction.reply({
+                embeds: [embed],
+                ephemeral: true
+            });
+
+            return;
+        }
+
+        if(gameRoom.creatorId == interaction.user.id){
+            const embed = new EmbedBuilder()
+                .setTitle('Uhoh!')
+                .setDescription('It seems you have tried to join your own room. Come on now.')
+                .setColor(EMBED_COLOR);
+
+            interaction.reply({
+                embeds: [embed],
+                ephemeral: true
+            });
+
+            return;
+        }
+
         if(gameRoom.joinedId != null){
             const embed = new EmbedBuilder()
                 .setTitle("Uhoh")
@@ -97,27 +123,29 @@ module.exports = {
             }
         });
 
+        const embed = new EmbedBuilder()
+            .setTitle("Why welcome there!")
+            .setDescription("You have been added to the channel. Have fun!")
+            .setColor(EMBED_COLOR);
+
+        interaction.reply({
+            embeds: [embed],
+            ephemeral: true
+        });
+
         //Add user to channel
         const channel = client.channels.cache.get(gameRoom.channelId);
         await channel.permissionOverwrites.edit(interaction.user.id, {ViewChannel: true});
 
-        const gameType = gameRoom.gameType;
-        let gameTitle;
-        switch(gameType){
-            case "rockpaperscissors":
-                gameTitle = "Rock Paper Scissors";
-                break;
-            case "battledome":
-                gameTitle = "Battle Dome";
-                break;
-            case "tictactoe":
-                gameTitle = "Tic Tac Toe";
-                break;
-        }
-
         const welcomeEmbed = new EmbedBuilder()
             .setTitle('Welcome!')
-            .setDescription(`Welcome to GameRoom. You are playing ${gameTitle}. To start the game run /play.`)
+            .setDescription(`Welcome to GameRoom. To start the game run one of these commands.`)
+            .setFields([
+                {
+                    name: "Tic Tac Toe",
+                    value: '/ttt {opponents mention}'
+                }
+            ])
             .setColor(EMBED_COLOR);
 
         //Stating who joined the game
